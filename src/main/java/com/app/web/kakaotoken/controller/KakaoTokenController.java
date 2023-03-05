436ad5bd5e30d5fcbe8cb4beb6c92db3661e53d2
dumbbelloper <dumbbelloper@gmail.com>
2023-03-05 18:42:42 +0900

    kakao login

--- src/main/java/com/app/web/kakaotoken/controller/KakaoTokenController.java
+++ src/main/java/com/app/web/kakaotoken/controller/KakaoTokenController.java
@@ -1,6 +1,9 @@
 package com.app.web.kakaotoken.controller;
 
+import com.app.web.kakaotoken.client.KakaoTokenClient;
+import com.app.web.kakaotoken.dto.KakaoTokenDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +11,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequiredArgsConstructor
 public class KakaoTokenController {
+
+    private final KakaoTokenClient kakaoTokenClient;
+
+    @Value("${kakao.client.id}")
+    private String clientId;
+
+
+    @Value("${kakao.client.secret}")
+    private String clientSecret;
+
+    @Value("${kakao.client.redirect-uri}")
+    private String redirectUri;
+
+
+
     @GetMapping("/login")
     public String login() {
         return "loginForm";
@@ -15,7 +33,17 @@ public class KakaoTokenController {
 
     @GetMapping("/oauth/kakao/callback")
     public @ResponseBody String loginCallback(String code) {
-        return code;
+        String contentType = "application/x-www-form-urlencoded;charset=utf-8";
+        KakaoTokenDto.Request kakaoTokenRequestDto = KakaoTokenDto.Request.builder()
+                .client_id(clientId)
+                .client_secret(clientSecret)
+                .grant_type("authorization_code")
+                .code(code)
+                .redirect_uri(redirectUri)
+                .build();
+
+        KakaoTokenDto.Response kakaoToken = kakaoTokenClient.requestKakaoToken(contentType, kakaoTokenRequestDto);
+        return "kakao token : " + kakaoToken;
     }
 
 }
