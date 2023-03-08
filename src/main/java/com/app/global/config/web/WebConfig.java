9cd9b61e1ac17ff5bbb8134e3fed4dbadf9a4ed4
dumbbelloper <dumbbelloper@gmail.com>
2023-03-08 22:32:11 +0900

    MemberInfoArgumentResolver 구현

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,18 +1,29 @@
 package com.app.global.config.web;
 
 import com.app.global.intercepter.AuthenticateInterceptor;
+import com.app.global.resolver.memberInfo.MemberInfoArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthenticateInterceptor authenticateInterceptor;
+    private final MemberInfoArgumentResolver memberInfoArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberInfoArgumentResolver);
+
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
