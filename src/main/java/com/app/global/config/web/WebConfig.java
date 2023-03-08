73c5d7ad7f28efb60800913a2d29abd9b9ed8451
dumbbelloper <dumbbelloper@gmail.com>
2023-03-08 21:58:47 +0900

    인증 인터셉터

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,11 +1,27 @@
 package com.app.global.config.web;
 
+import com.app.global.intercepter.AuthenticateInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticateInterceptor authenticateInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticateInterceptor)
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/oauth/login", "api/access-token/issue", "/api/logout", "api/health");
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
